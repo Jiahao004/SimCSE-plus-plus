96699f382fa0ed127cfe9870b2d1de80df9db63d
Xu Jiahao <76723662+Jiahao004@users.noreply.github.com>
2022-09-25 18:02:35 +0800

    Update run_unsup_example.sh

--- run_unsup_example.sh
+++ run_unsup_example.sh
@@ -3,9 +3,6 @@
 # In this example, we show how to train SimCSE on unsupervised Wikipedia data.
 # If you want to train it with multiple GPU cards, see "run_sup_example.sh"
 # about how to use PyTorch's distributed data parallel.
-export http_proxy="http://sys-proxy-rd-relay.byted.org:8118"
-export https_proxy="http://sys-proxy-rd-relay.byted.org:8118"
-export no_proxy="byted.org,bytedance.net,.byted.org,.bytedance.net,localhost,127.0.0.1,::1,10.0.0.0/8,127.0.0.0/8,fd00::/8,100.64.0.0/10,fe80::/10,172.16.0.0/12,169.254.0.0/16,192.168.0.0/16"
 
 
 
